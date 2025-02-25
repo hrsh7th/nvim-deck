0b4621f209be74c3185782b8d19c0307351d1d91
hrsh7th <629908+hrsh7th@users.noreply.github.com>
2025-02-25 20:20:00 +0900

    debug

--- lua/deck/builtin/source/explorer/init.spec.lua
+++ lua/deck/builtin/source/explorer/init.spec.lua
@@ -15,15 +15,15 @@ end)()
 
 ---Ensure fixture directory.
 local function setup()
-  local fixture_dir = vim.fs.joinpath(debug.getinfo(1, 'S').source:sub(2):match('(.*/)'), '../../../../../fixtures/fs')
   vim.print({
     fixture_target_dir = fixture_target_dir,
-    fixture_dir = fixture_dir,
+    debugpath = debug.getinfo(1, 'S').source:sub(2)
   })
   return Async.run(function()
     IO.rm(fixture_target_dir, { recursive = true }):catch(function()
     end):await()
 
+    local fixture_dir = vim.fs.joinpath(debug.getinfo(1, 'S').source:sub(2):match('(.*/)'), '../../../../../fixtures/fs')
     IO.cp(fixture_dir, fixture_target_dir, { recursive = true }):await()
     vim.print({
       fixture_target_dir = vim.uv.fs_stat(fixture_target_dir),
